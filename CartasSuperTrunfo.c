7c4daf6b25ed12e51a1f80f2905bb8e838120c5e
robertokuta <robertouta@gmail.com>
2025-03-16 15:00:15 +0000

    EXIBINDO NA TELA AS VARIAVEIS

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,38 +10,38 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-        int estado; //VARIÁVEL QUE CONTERÁ O ESTADO
-        char cod_carta[3]; // VARIÁVEL CÓDIGO DA CARTA
+        int estado;   //VARIÁVEL QUE CONTERÁ O ESTADO
+        char cod_carta[3];  // VARIÁVEL CÓDIGO DA CARTA
         char nome_cidade[20]; // VARIÁVEL NOME DA CIDADE
-        int populacao; // VARIÁVEL POPULAÇÃO
-        float area; // VARIÁVEL ÁREA
-        float pib; // VARIÁVEL PIB
-        int pontos_turisticos; // VARIÁVEL PONTOS TURÍSTICOS
+        int populacao;  // VARIÁVEL POPULAÇÃO
+        float area;  // VARIÁVEL ÁREA
+        float pib;  // VARIÁVEL PIB
+        int pontos_turisticos;  // VARIÁVEL PONTOS TURÍSTICOS
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
-    printf("Digite o Estado: \n");
-    scanf ("%d", &estado);
+    printf("Digite o Estado:"); //EXIBE NA TELA 'DIGITE ESTADO
+    scanf ("%d", &estado); //CAPTURA INFORMAÇÃO P/ VARIÁVEL 'INT ESTADO'
 
-    printf("Digite o Códido da Carta: \n");
-    scanf ("%s", &cod_carta);
+    printf("Digite o Códido da Carta:\n"); // EXIBE NA TELA 'CÓDIGO DA CARTA'
+    scanf ("%s", &cod_carta); // CAPTURA INFORMAÇAO P/ VARIÁVEL 'COD_CARTA'
 
-    printf ("Digite o nome da cidade: \n");
-    scanf ("%s", &nome_cidade);
+    printf ("Digite o nome da cidade:\n"); // EXIBE NA TELA 'NOME DA CIDADE'
+    scanf ("%s", &nome_cidade); // CAPTURA INFORMAÇÃO P/ VARIAVEL 'NOME_CIDADE'
 
-    printf ("Digite a População: \n");
-    scanf ("%d", &populacao);
+    printf ("Digite a População: \n"); //EXIBE NA TELA 'POPULAÇÃO'
+    scanf ("%d", &populacao); //CAPTURA VARIÁVEL POPULAÇAO
 
-    printf ("Digite a Área: \n");
-    scanf ("%f", &area);
+    printf ("Digite a Área: \n"); //EXIBE NA TELA 'DIGITE A ÁREA'
+    scanf ("%f", &area); // CAPTURA VARIÁVEL 'AREA''
 
-    printf ("Digite o PIB: \n");
-    scanf ("%f", &pib);
+    printf ("Digite o PIB: \n"); //EXIBE NA TELA 'DIGITE O PIB'
+    scanf ("%f", &pib); //CAPTURA VARIÁVEL 'PIB'
 
-    printf ("Digite a quantidade de Pontos Turísticos: \n");
-    scanf ("%d", &pontos_turisticos);
+    printf ("Digite a quantidade de Pontos Turísticos: \n"); // EXIBE NA TELA 'QUANTIDADE DE PONTOS TURISTICOS'
+    scanf ("%d", &pontos_turisticos); // CAPUTRA VARIÁVEL 'PONTOS_TURISTICOS'
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
